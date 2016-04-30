f59c6cb9469fef41263887f4e0918950ccefd114
Iman Tabrizian <tabrizian@outlook.com>
2016-05-01 00:20:31 +0430

    Adds comparison sample code.

---
+++ PHP/basic/comparison.php
@@ -0,0 +1,34 @@
+<?php
+
+class Box {
+    public $name="box";
+}
+$box = new Box();
+$box_reference = $box;
+$box_clone = clone $box;
+
+$box_changed = clone $box;
+$box_changed-> name = "changed box";
+
+$another_box = new Box();
+
+// == is casual and just checks to see if attributes are the same
+
+echo $box == $box_reference ? 'true' : 'false'; //true
+echo "<br />";
+echo $box == $box_clone ? 'true' : 'false';     //true
+echo "<br />";
+echo $box == $box_changed ? 'true' : 'false';  // false
+echo "<br />";
+echo $box == $another_box ? 'true' : 'false'; // true
+echo "<br />"."<br />";
+
+echo $box === $box_reference ? 'true' : 'false'; // true
+echo "<br />";
+echo $box === $box_clone ? 'true' : 'false'; //false
+echo "<br />";
+echo $box === $box_changed ? 'true' : 'false'; //false
+echo "<br />";
+echo $box === $another_box ? 'true' : 'false'; //false
+echo "<br />"."<br />";
+?>
