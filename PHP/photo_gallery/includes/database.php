e52b4634a756eeacb75fcc667885cc88a5871db2
Iman Tabrizian <tabrizian@outlook.com>
2016-05-01 21:14:31 +0430

    Adds database.php.

---
+++ PHP/photo_gallery/includes/database.php
@@ -0,0 +1,43 @@
+<?php
+require_once("config.php");
+
+class MySQLDatabase {
+
+    private $connection;
+
+    function __construct() {
+        $this->open_connection();
+    }
+
+    public function open_connection() {
+        $this->$connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+        if(!$this->$connection) {
+            die("Database connection failed: " . mysql_error());
+        } else {
+            $db_select = mysql_select_db(DB_NAME, $this->connection);
+            if(!$db_select) {
+                die("Database selection failed " . mysql_error());
+            }
+        }
+    }
+
+    public function query($sql) {
+        $result = mysql_error($sql, $this->connection);
+        if(!$result) {
+            die("Database query failed: ". mysql_error());
+        }
+
+        return $result;
+    }
+
+    public function close_connection() {
+        if(isset($this->connection)) {
+            mysql_close($this->connection);
+            unset($this->connection);
+        }
+    }
+}
+
+$database = new MySQLDatabase();
+$db = &$database;
+?>
