c959bc421eeeb5519c189f56a3aecd6e750d0f53
Iman Tabrizian <tabrizian@outlook.com>
2016-05-03 21:46:28 +0430

    Improves database.php

--- PHP/photo_gallery/includes/database.php
+++ PHP/photo_gallery/includes/database.php
@@ -10,14 +10,14 @@ class MySQLDatabase {
     }
 
     public function open_connection() {
-        $this->$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-        if(!$this->$connection) {
+        $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+        if(!$this->connection) {
             die("Database connection failed: " . mysqli_error($this->connection));
         }
     }
 
     public function query($sql) {
-        $result = mysqli_error($this->connection, $sql);
+        $result = mysqli_query($this->connection, $sql);
         $this->confirm_query($result);
         return $result;
     }
@@ -29,8 +29,24 @@ class MySQLDatabase {
         }
     }
 
-    public function mysql_prep($value) {
+    public function escape_value($value) {
         return mysqli_real_escape_string($this->connection ,$value);
+    }
+
+    public function fetch_array($result_set) {
+        return mysqli_fetch_assoc($result_set);
+    }
+
+    public function num_rows($result_set) {
+        return mysqli_num_rows($result_set);
+    }
+
+    public function inserted_id() {
+        return mysqli_insert_id($this->connection);
+    }
+
+    public function affected_rows() {
+        return mysqli_affected_rows($this->connection);
     }
 
     private function confirm_query($result) {
--- PHP/photo_gallery/public/index.php
+++ PHP/photo_gallery/public/index.php
@@ -1,5 +1,12 @@
 <?php
+require_once("../includes/database.php");
+if(isset($database)) { echo "true"; } else { echo "false";}
+echo "<br />";
 
-echo "Is this working?";
+echo $database->escape_value("It's working?<br />");
 
+$sql = "SELECT * FROM users WHERE id = 1";
+$result_set = $database->query($sql);
+$found_user = $database->fetch_array($result_set);
+echo $found_user['username'];
 ?>
