ee225fee3ec877d1f47a30b535824c3fddfceeef
Iman Tabrizian <tabrizian@outlook.com>
2016-05-03 23:44:01 +0430

    Improves user class.

--- PHP/photo_gallery/includes/user.php
+++ PHP/photo_gallery/includes/user.php
@@ -3,15 +3,20 @@
 require_once('database.php');
 
 class User {
+
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+
     public static function find_all() {
-        global $database;
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
+        return self::find_by_sql("SELECT * FROM users");
     }
 
     public static function find_by_id($id = 0) {
         global $database;
-        $result_set = $database->query("SELECT * FROM users WHERE
+        $result_set = self::find_by_sql("SELECT * FROM users WHERE
             id={$id} LIMIT 1");
         $found = $database->fetch_array($result_set);
         return $found;
@@ -21,6 +26,24 @@ class User {
         global $database;
         $result_set = $database->query($sql);
         return $result_set;
+    }
+
+    public function full_name() {
+        if(isset($this->first_name) && isset($this->last_name)) {
+            return $this->first_name . " " . $this->last_name;
+        } else {
+            return "";
+        }
+    }
+
+    private static function instantiate($record) {
+        $object = new self;
+        $user-> id = $record['id'];
+        $user-> username = $record['username'];
+        $user-> password = $record['password'];
+        $user-> first_name = $record['first_name'];
+        $user-> last_name = $record['last_name'];
+        return $object;
     }
 
 }
--- PHP/photo_gallery/public/index.php
+++ PHP/photo_gallery/public/index.php
@@ -1,23 +1,21 @@
 <?php
 require_once("../includes/database.php");
 require_once("../includes/user.php");
-if(isset($database)) { echo "true"; } else { echo "false";}
-echo "<br />";
 
-echo $database->escape_value("It's working?<br />");
-
-$sql = "SELECT * FROM users WHERE id = 1";
-$result_set = $database->query($sql);
-$found_user = $database->fetch_array($result_set);
-echo $found_user['username'];
+$record = User::find_by_id(1);
+$user = new User();
+$user-> id = $record['id'];
+$user-> username = $record['username'];
+$user-> password = $record['password'];
+$user-> first_name = $record['first_name'];
+$user-> last_name = $record['last_name'];
+echo $user->full_name();
 
 echo "<hr />";
-$found_user = User::find_by_id(1);
-echo $found_user['username'];
 
-$user_set = User::find_all();
-while($user = $database->fetch_array($user_set)) {
-    echo "User: ". $user['username'] . "<br />";
-    echo "Name: ". $user['first_name'] . " " . $user['last_name']. "<br /><br />";
-}
+//$user_set = User::find_all();
+//while($user = $database->fetch_array($user_set)) {
+//    echo "User: ". $user['username'] . "<br />";
+//    echo "Name: ". $user['first_name'] . " " . $user['last_name']. "<br /><br />";
+//}
 ?>
