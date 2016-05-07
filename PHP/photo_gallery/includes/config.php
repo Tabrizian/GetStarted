1b48cc3a12b0be458c492da33ea9c46c2e01d8fc
Iman Tabrizian <tabrizian@outlook.com>
2016-05-08 00:27:59 +0430

    Changes config.

--- PHP/photo_gallery/includes/config.php
+++ PHP/photo_gallery/includes/config.php
@@ -3,7 +3,7 @@
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 defined('DB_USER') ? null : define("DB_USER", "gallery");
-defined('DB_PASS') ? null : define("DB_PASS", "iman11");
+defined('DB_PASS') ? null : define("DB_PASS", "12345");
 defined('DB_NAME') ? null : define("DB_NAME", "photo_gallery");
 
 ?>
