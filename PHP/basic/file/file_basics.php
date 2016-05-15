2c3705d380f6532941f6921198a5cfca65f9e19c
Iman Tabrizian <tabrizian@outlook.com>
2016-05-15 21:42:17 +0430

    Improves file_basics.php

--- PHP/basic/file/file_basics.php
+++ PHP/basic/file/file_basics.php
@@ -1,5 +1,26 @@
 <?php
 
-echo __FILE__."<br />";
+echo __FILE__ ."<br />";
+echo __LINE__ ."<br />"; // be careful once you include files
+echo dirname(__FILE__). "<br />";
+echo __DIR__ ."<br />";
 
+echo file_exists(__FILE__) ? 'yes' : 'no';
+echo "<br />";
+echo file_exists(dirname(__FILE__)."/file_basics.php") ? 'yes' : 'no';
+echo "<br />";
+echo file_exists(dirname(__FILE__)) ? 'yes' : 'no';
+echo "<br />";
+
+echo is_file(dirname(__FILE__)."/file_basics.php") ? 'yes' : 'no';
+echo "<br />";
+echo is_file(dirname(__FILE__)) ? 'yes' : 'no';
+echo "<br />";
+
+echo is_dir(dirname(__FILE__)."/file_basics.php") ? 'yes' : 'no';
+echo "<br />";
+echo is_dir(dirname(__FILE__)) ? 'yes' : 'no';
+echo "<br />";
+echo is_dir('..') ? 'yes' : 'no';
+echo "<br />";
 ?>
