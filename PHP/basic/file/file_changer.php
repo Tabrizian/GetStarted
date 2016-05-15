7186e5aa9ae7c63618dada344ddb022c8ebf9eb3
Iman Tabrizian <tabrizian@outlook.com>
2016-05-16 01:21:53 +0430

    Adds some file related tutorials.

--- PHP/basic/file/file_changer.php
+++ PHP/basic/file/file_changer.php
@@ -1,5 +1,36 @@
 <?php
 
 echo fileowner('file_permissions.php');
+echo "<br />";
 
+// if we have Posix installed
+
+$owner_id = fileowner('file_permissions.php');
+$owner_array = posix_getpwuid($owner_id);
+echo $owner_array['name'];
+
+echo "<br />";
+
+chown('file_permissions.php', 'iman');
+// chown only works is PHP is superuser
+// making webserver/PHP a superuser is a big security issue
+
+// if we have Posix installed
+
+$owner_id = fileowner('file_permissions.php');
+$owner_array = posix_getpwuid($owner_id);
+echo $owner_array['name'];
+
+echo "<br />";
+
+echo substr(decoct(fileperms('file_permissions.php')), 2);
+echo "<br />";
+chmod('file_permissions.php', 0777);
+echo substr(decoct(fileperms('file_permissions.php')), 2);
+
+echo "<br />";
+echo is_readable('file_permissions.php') ? 'yes' : 'no';
+echo "<br />";
+echo is_writable('file_permissions.php') ? 'yes' : 'no';
+echo "<br />";
 ?>
