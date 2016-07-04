92ec12e887b6894ad285b5e2fd31b945697181fd
Iman Tabrizian <tabrizian@outlook.com>
2016-07-04 11:08:34 +0430

    Improves routing

---
+++ PHP/laravel/learning-laravel-5/resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+<h1>About Me : {{ $first }} {{$last}} </h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius itaque magnam maiores minima neque nesciunt nisi non
+    odit, pariatur possimus, quae quaerat reprehenderit sit. At dolor dolore est et</p>
+@stop
---
+++ PHP/laravel/learning-laravel-5/resources/views/pages/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+@section('content')
+    <h2>Contact Me!</h2>
+@stop
+
+@section('footer')
+    <script>alert('Contact form scripts');</script>
+@stop
