10d1ff9d1b53103ac321c54bfc8c903d6a3b92ab
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-25 23:04:55 +0430

    Added some validations

--- PHP/laravel/learning-laravel-5/app/Http/Controllers/NotesController.php
+++ PHP/laravel/learning-laravel-5/app/Http/Controllers/NotesController.php
@@ -10,19 +10,28 @@ use App\Http\Requests;
 
 class NotesController extends Controller
 {
-    public function store(Request $request, Card $card) {
+    public function store(Request $request, Card $card)
+    {
+
+        $this->validate($request, [
+            'body' => 'required|min:10'
+        ]);
+
         $note = new Note();
+        $note->user_id = 1;
         $note->body = $request->body;
         $card->notes()->save($note);
 
         return back();
     }
 
-    public function edit(Note $note){
+    public function edit(Note $note)
+    {
         return view('notes.edit', compact('note'));
     }
 
-    public function update(Note $note, Request $request) {
+    public function update(Note $note, Request $request)
+    {
         $note->update(['body' => $request->body]);
 
         return back();
--- PHP/laravel/learning-laravel-5/resources/views/cards/show.blade.php
+++ PHP/laravel/learning-laravel-5/resources/views/cards/show.blade.php
@@ -28,14 +28,22 @@
 
             <h3>Add a new note</h3>
             <form method="post" action="/cards/{{ $card->id }}/notes">
-                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                {{ csrf_field() }}
                 <div class="form-group">
-                    <textarea name="body" class="form-control"></textarea>
+                    <textarea name="body" class="form-control">{{ old('body') }}</textarea>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Add Note</button>
                 </div>
             </form>
+
+            @if (count($errors))
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
         </div>
     </div>
