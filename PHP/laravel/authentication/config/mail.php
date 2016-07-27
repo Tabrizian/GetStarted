b30abc3ca68d322e01599a8edfbcb6a44a50fceb
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-27 14:31:31 +0430

    Improves home middleware

--- PHP/laravel/authentication/config/mail.php
+++ PHP/laravel/authentication/config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'tabrizian@outlook.com', 'name' => 'Iman Tabrizian'],
 
     /*
     |--------------------------------------------------------------------------
