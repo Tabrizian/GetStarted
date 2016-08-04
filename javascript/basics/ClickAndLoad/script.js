d281676cb3c3863e8b0d86119195457108f58159
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-04 20:44:37 +0430

    Adds click and load sample code

---
+++ javascript/basics/ClickAndLoad/script.js
@@ -0,0 +1,30 @@
+document.onclick = function () {
+    alert("You clicked somewhere in the document!");
+};
+
+
+function prepareEventHandlers() {
+    var myImage = document.getElementById("mainImage");
+    myImage.onclick = function () {
+        alert("You clicked image!");
+    };
+}
+
+window.onload = prepareEventHandlers;
+// use the commented-out code for reference, or just write your own. 
+
+//document.onclick = function() {
+//	alert("You clicked somewhere in the document");
+//};
+
+//function prepareEventHandlers() {	
+//	var myImage = document.getElementById("mainImage");
+//	myImage.onclick =  function() {
+//		alert("You clicked the image");
+//	}
+//}
+
+//window.onload = function() {
+// prep anything we need to
+//	prepareEventHandlers();
+//};
