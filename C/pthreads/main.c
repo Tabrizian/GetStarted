edc0772ceabda017b4a3e377d5ea2fd28e2f7062
Iman Tabrizian <Tabrizian@outlook.com>
2016-09-01 01:06:56 +0430

    Improves code formatting

--- C/pthreads/main.c
+++ C/pthreads/main.c
@@ -16,7 +16,7 @@ int main (int argc, char *argv[])
    pthread_t threads[NUM_THREADS];
    int rc;
    long t;
-   for(t=0; t < NUM_THREADS; t++){
+   for(t = 0; t < NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t);
       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
       if (rc){
