2323be7d25f17335ec9c3b845dec706e625830a8
Iman Tabrizian <tabrizian@outlook.com>
2018-02-02 16:06:29 +0330

    Improve Go tutorial

---
+++ Go/basics/fmt/consoleOutput.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    str1 := "The quick red fox"
+    str2 := "jumped over"
+    str3 := "the lazy brown dog"
+
+    aNumber := 42
+    isTrue := true
+
+    stringLength, _ := fmt.Println(str1, str2, str3)
+
+    // if err == nil {
+        fmt.Println("String length:", stringLength)
+    // }
+
+    fmt.Printf("Value of aNumber: %v\n", aNumber)
+    fmt.Printf("Value of isTrue: %v\n", isTrue)
+    fmt.Printf("Value of isTrue: %.2f\n", float64 (aNumber))
+    fmt.Printf("Data types: %T, %T, %T, %T and %T\n", str1, str2, str3, aNumber, isTrue)
+    myString := fmt.Sprintf("Data types as var: %T, %T, %T, %T and %T", str1, str2, str3, aNumber, isTrue)
+
+    fmt.Printf(myString)
+
+}
---
+++ Go/basics/strings/strings.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str1 := "An implicitly typed string"
+	fmt.Println(str1)
+}
